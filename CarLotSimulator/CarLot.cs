4c8c4208e755c75b229735b5ffd07de0d7d370f7
Yun Jang <jang.yunj7@gmail.com>
2023-02-26 23:39:35 -0500

    added carlot class

---
+++ CarLotSimulator/CarLot.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarLotSimulator
+{
+    internal class CarLot
+    {
+        public List<Car> CarList { get; set; } = new List<Car>(); //OR var CarList {get;set;} = new List<Car>();  
+
+        public static int numberOfCars = 0;
+
+    }
+
+
+
+}
