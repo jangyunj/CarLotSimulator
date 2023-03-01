568d9bd93c7fc71e39ff646f0e4da3e8752a038a
Yun Jang <jang.yunj7@gmail.com>
2023-02-28 23:41:50 -0500

    final commit

--- CarLotSimulator/Program.cs
+++ CarLotSimulator/Program.cs
@@ -56,4 +56,27 @@ namespace CarLotSimulator
             var car3 = new Car("Jeep", "Wrangler", 2022);
             Carlot.CarList.Add(car3);
 
-           
+
+            //*************BONUS*************//
+
+            // Set the properties utilizing the 3 different ways we learned about, one way for each car
+
+            //*************BONUS X 2*************//
+
+            //Create a CarLot class
+            //It should have at least one property: a List of cars
+            //Instanciate the a Carlot at the beginning of the program and as you create a car add the car to the list.
+            //At the end iterate through the list printing each of car's Year, Make, and Model to the console
+            foreach (var item in Carlot.CarList)
+            {
+                Console.WriteLine(item.Year);
+                Console.WriteLine(item.Make);
+                Console.WriteLine(item.Model);
+                item.MakeEngineNoise("Vroom");
+                item.MakeHonkNoise("Beep");
+            }
+
+
+        }
+    }
+}
